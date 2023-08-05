17327a3c2fc0e7f00ac9ecb5540b3253f8c89909
Minikot88 <alongkot000@gmail.com>
2023-08-05 19:24:20 +0700

    m2

--- src/view/select-subject.js
+++ src/view/select-subject.js
@@ -25,16 +25,16 @@ import './detail.css'
 
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+function createData(name, sec, fat, carbs, protein) {
+    return { name, sec, fat, carbs, protein };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt',  1, 24, 4.0),
+    createData('Ice cream sandwich',  2, 37, 4.3),
+    createData('Eclair',  3, 24, 6.0),
+    createData('Cupcake',  4, 67, 4.3),
+    createData('Gingerbread',  5, 49, 3.9),
 ];
 
 const theme = createTheme();
@@ -145,7 +145,7 @@ export default function SelectSubject() {
                                 </TableCell>
 
                                 <TableCell align="center" component="th" scope="row">
-                                    {row.fat}
+                                    {row.sec}
                                 </TableCell>
 
                                 <TableCell align="center">
