ec84a966653d0b398023250cd7bd64d3fe3e4f98
Minikot88 <alongkot000@gmail.com>
2023-08-08 23:28:13 +0700

    mm2

--- src/view/home-page.js
+++ src/view/home-page.js
@@ -9,8 +9,6 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import Appbar from '../component/app-bar';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const theme = createTheme();
 
 export default function Home() {
